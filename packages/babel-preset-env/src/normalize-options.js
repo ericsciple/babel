e3f8c9f464c97fcbb73cf4196eeb8fff8e2e58d4
Huáng Jùnliàng <jlhwung@gmail.com>
2019-12-19 17:15:36 -0500

    chore: remove out-dated todo item

--- packages/babel-preset-env/src/normalize-options.js
+++ packages/babel-preset-env/src/normalize-options.js
@@ -120,7 +120,6 @@ export const checkDuplicateIncludeExcludes = (
 };
 
 const normalizeTargets = targets => {
-  // TODO: Allow to use only query or strings as a targets from next breaking change.
   if (isBrowsersQueryValid(targets)) {
     return { browsers: targets };
   }
